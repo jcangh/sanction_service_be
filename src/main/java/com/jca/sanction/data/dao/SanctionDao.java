a2f5d08b5785bd6b78c90410e445e7a92887aa4d
Juan Angulo <juan-carlos.angulo@endava.com>
2023-08-25 20:11:18 -0500

    improve activate/reject queries when events are triggered

--- src/main/java/com/jca/sanction/data/dao/SanctionDao.java
+++ src/main/java/com/jca/sanction/data/dao/SanctionDao.java
@@ -1,8 +1,19 @@
 package com.jca.sanction.data.dao;
 
 import com.jca.sanction.data.entity.SanctionEntity;
+import com.jca.sanction.enums.SanctionEvent;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Map;
+
 @Repository
-public interface SanctionDao extends CrudRepository<SanctionEntity,String> {}
+public interface SanctionDao extends CrudRepository<SanctionEntity,String> {
+
+    @Query("SELECT s.additionalValues FROM SanctionEntity s WHERE s.id = :sanctionId")
+    Map<String, Object> getAdditionalValues(String sanctionId);
+
+    @Query("SELECT count(s.id) FROM SanctionEntity s JOIN s.actions a WHERE a.event = :sanctionEvent")
+    Integer countSanctionEvents(SanctionEvent sanctionEvent);
+}
